30a883af4c2769f086b0826b1cf6087e23104fff
sam190291 <sam@leadhq.com>
2025-08-01 00:19:06 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    fallback: {
+      "crypto": false,
+      "stream": false,
+      "buffer": false,
+      "process": false
+    }
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    port: 3000
+  }
+};
